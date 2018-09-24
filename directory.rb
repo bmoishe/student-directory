fd00a6f0e66d959da366b0dc979e5484c7f8de09
Moishe <moishebarnes@gmail.com>
2018-09-24 11:50:50 +0100

    gets.chomp

--- directory.rb
+++ directory.rb
@@ -1,18 +1,23 @@
 #lets' put all students into an array
-students = [
-{:name => "Dr Hannibal Lecter", :cohort => "november"},
-{:name => "Darth Vader", :cohort => "november"},
-{:name => "Nurse Ratched", :cohort => "november"},
-{:name => "Michael Corleone", :cohort => "november"},
-{:name => "Alex DeLarge", :cohort => "november"},
-{:name => "The Wicked Witch of the West", :cohort => "november"},
-{:name => "Terminator", :cohort => "november"},
-{:name => "Freddy Krueger", :cohort => "november"},
-{:name => "The Joker", :cohort => "november"},
-{:name => "Joffrey Baratheon", :cohort => "november"},
-{:name => "Norman Bates", :cohort => "november"}
-]
-#and then print them
+def input_students
+  puts "Please enter the names of the students"
+  puts "To finnish, jusr hit return twice"
+  #creates an empty Array
+  students = []
+  # get the first name
+  name = gets.chomp
+  # while the ame is not empty, repeat this code
+  while !name.empty? do
+  # add the student hash to the array
+  students << {name: name, cohort: :november}
+  puts "Now we have #{students.count} students"
+  # get another name from user
+  name = gets.chomp
+  end
+  # return the array of the students
+  students
+end
+
 def print_header
   puts "The student of Villains Academy"
   puts "------------"
@@ -28,6 +33,7 @@ def print_footer(students)
   puts "Overall, we have #{students.count} great students"
 end
 
+students = input_students
 print_header
 print(students)
 print_footer(students)
