eb2d3e68517dbbbb0b3a1dd1f5d6ab5424692bad
Moishe <moishebarnes@gmail.com>
2018-09-25 11:49:33 +0100

    Step 10

--- directory.rb
+++ directory.rb
@@ -1,3 +1,27 @@
+def interactive_menu
+  students = []
+  loop do
+    puts "1. Input the students"
+    puts "2. Show the students"
+    puts "3. Exit" # 9 because we'll be adding more items
+    selection = gets.chomp
+    case selection
+    when '1'
+      students = input_students
+      #input students
+    when '2'
+      print_header
+      print(students)
+      print_footer(students)
+      # Show the students
+    when '3'
+        exit  # Exit
+    else
+        puts 'I dont know what you meant, try again'
+    end
+  end
+end
+
 #lets' put all students into an array
 def input_students
   puts "Please enter the names of the students"
@@ -33,7 +57,4 @@ def print_footer(students)
   puts "Overall, we have #{students.count} great students"
 end
 
-students = input_students
-print_header
-print(students)
-print_footer(students)
+interactive_menu
